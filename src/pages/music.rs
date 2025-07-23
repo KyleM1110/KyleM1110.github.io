69073b31b966d1f5bdc6eee0af6b0386abfeac83
Dylan Manuel <dylan.manuel@att.net>
2025-07-22 20:15:19 -0500

    chore: load test data

--- src/pages/music.rs
+++ src/pages/music.rs
@@ -5,7 +5,9 @@ use crate::components::LinkButton;
 
 async fn fetch_monkeys() -> String {
     // maybe this didn't need to be async
-    let body = reqwest::get("http://[::1]:8080/images/kyle.png")
+    let url = format!("{}/data/test.json", window().location().origin().unwrap());
+    log::info!("{}", url);
+    let body = reqwest::get(url)
         .await
         .unwrap()
         .text()
@@ -25,7 +27,7 @@ pub fn Music() -> impl IntoView {
         >
             // Suspend allows you use to an async block in the view
             <p>
-                "Your shouting name is "
+                "Your test data is "
                 {move || Suspend::new(async move {
                     async_data.await
                 })}
